a7fd3ac5b39aad2dfe14c6d9e38371a041c5fa14
davve <davve@qq.com>
2020-10-28 11:18:13 +0800

    add edit

--- router/admin.go
+++ router/admin.go
@@ -18,8 +18,8 @@ func AdminApi(router *gin.RouterGroup) {
 		adminGroup.GET("admin/goods/specs/:spec_id/values", admin.SpecsValues)           // done
 		adminGroup.POST("admin/goods/specs/:spec_id/values", admin.UpdateSpecsValues)    // done
 		adminGroup.GET("admin/goods", admin.GoodsList)                                   // done
-		adminGroup.PUT("admin/goods/:goods_id/up", admin.GoodsUp)                        // 商品上架
-		adminGroup.PUT("admin/goods/:goods_id/under", admin.GoodsUnder)                  // 商品下架
+		adminGroup.PUT("admin/goods/:goods_id/up", admin.GoodsUp)                        // done
+		adminGroup.PUT("admin/goods/:goods_id/under", admin.GoodsUnder)                  // done
 		adminGroup.GET("admin/goods/brands", admin.BrandList)                            // done
 		adminGroup.POST("admin/goods/brands", admin.CreateBrand)                         // done
 		adminGroup.GET("admin/goods/brands/:brand_id", admin.Brand)                      // done
